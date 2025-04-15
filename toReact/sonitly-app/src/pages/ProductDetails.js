31f144fc72770c6122512592ba5f1191f4850da0
ash-a9236 <helene.m.rousseau@gmail.com>
2025-04-15 14:26:59 -0400

    product details page almost done

--- toReact/sonitly-app/src/pages/ProductDetails.js
+++ toReact/sonitly-app/src/pages/ProductDetails.js
@@ -1,42 +1,98 @@
 const ProductDetails = () => {
   return (
-    <div id="backgroundContainer">
-      <div id="productImage">
-          <div id="imageSize">
-              <p>productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br />
-                  productImage<br /></p>
-          </div>
-      </div>
+    <div id="tempContainer">
+    <div id="baseInformationContainer">
+        <img id="productImageFullP"
+        src="cat.jpg"
+        alt="product image"/>
 
-      <div id="productRating">
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <div class="circle"></div>
-      </div>
-        
-        <div id="productDescription">
-          <h2>Book Title by Author</h2>
-          <p><br />This is a book description with the resume and everything</p>
+        <div id="carrousselIndicator">            
+            <span class="carrousselIndicatorItem"></span> <br/>
+            <span class="carrousselIndicatorItem"></span> <br/>
+            <span class="carrousselIndicatorItem"></span> <br/>
         </div>
 
-        <br />
-        <br />
+        <div id="baseItemText">
+            <h1 id="itemTitle">L This is the title</h1>
+            <h3 id="itemAuthor">Author author</h3>
+            
+            <div id="tagsContainer">
+                <span class="itemTags">Genre</span>
+                <span class="itemTags">Another Genre</span>
+                <span class="itemTags">Genre3</span>
+                <span class="itemTags">Really loonnng genre</span>
+                <span class="itemTags">Overflowing genre</span>
+            </div>
+        </div>
     </div>
+
+    <div id="connectingDiv">
+        <span class="buyOptions">Edition *</span>
+        <span class="buyOptions">Type *</span>
+        <span class="buyOptions">Retailer *</span>
+        <span class="socialPlaceholderTemp"></span>
+        <span class="socialPlaceholderTemp"></span>
+        <span class="socialPlaceholderTemp"></span>
+    </div>
+
+    <div id="additionalInformationContainer">
+
+        <div id="additionalItemInformation">
+            <h3 id="editor">Edition House</h3>
+            <p>Thats an edition house</p>
+
+            <h3 id="funStats">Statistics</h3>
+            <p>Word Count : 0000</p>
+            <p>Page Count : 000</p>
+            <p>Chapters : 000</p>
+            <p>Editions : 000</p>
+
+            <h3 id="itemLanguages">Languages</h3>
+            <p>American English</p>
+            <p>French</p>
+        </div>
+
+        <div id="itemFullDescription">
+            <h3 id="description">Description</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                <br/><br/> Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.</p>
+        </div> 
+    </div>
+
+    <h2 id="alsoLikeTitle">You might also like...</h2>
+    <br/>
+
+    <div id="furtherStuff">
+        <img class="productImageSmall"
+        src="anothercat.jpg"
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src="cat.jpg"
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src="anothercat.jpg"
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src="cat.jpg"
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src="anothercat.jpg"
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src="cat.jpg"
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src="anothercat.jpg"
+        alt="product image"/>
+    </div>
+</div>
+
   );
 }
 
