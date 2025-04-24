770426de11963922c23df1f0f3b272975767d1b3
ash-a9236 <helene.m.rousseau@gmail.com>
2025-04-24 16:14:36 -0400

    product details almost done : only small adjusts left

--- toReact/sonitly-app/src/pages/ProductDetails.js
+++ toReact/sonitly-app/src/pages/ProductDetails.js
@@ -37,99 +37,91 @@ const ProductDetails = () => {
     if (loading) return <div>Loading product details...</div>;
     if (!product) return <div>Product not found.</div>;
   return (
-    <div id="productContainers">
-        <div id="baseInformationContainer">
-            <img id="productImageFullP"
-            src={Image}
-            alt="product image"/>
+    <>
+      <div id="baseProductInfo">
+        <span id="leftInfo">
+          <span id="CButtons">
+              <button class="carousselButton"></button>
+              <button class="carousselButton"></button>
+              <button class="carousselButton"></button>
+          </span>
 
-            <div id="carrousselIndicator">            
-                <span className="carrousselIndicatorItem"></span> <br/>
-                <span className="carrousselIndicatorItem"></span> <br/>
-                <span className="carrousselIndicatorItem"></span> <br/>
-            </div>
+          <img id="productImage"
+          src={Image}
+          alt="product image"/>
+      </span>
 
-            <div id="baseItemText">
-                <h1 id="itemTitle">{product?.item_title}</h1>
-                <h3 id="itemAuthor">{product?.brand}</h3>
-                
-                <div id="tagsContainer">
-                    <span className="itemTags">{product?.category_id}</span>
-                    <span className="itemTags">Another Genre</span>
-                    <span className="itemTags">Genre3</span>
-                    <span className="itemTags">Really loonnng genre</span>
-                    <span className="itemTags">Overflowing genre</span>
-                </div>
-            </div>
-        </div>
+        <span id="rightInfo">
+          <h1 id="itemTitle">{product?.item_title}</h1>
+          <h3 id="itemAuthor">{product?.brand}</h3>
+          
+          <div id="tagsContainer">
+              <span class="itemTags">{product?.category_id}</span>
+              <span class="itemTags">Another Genre</span>
+              <span class="itemTags">Genre3</span>
+              <span class="itemTags">Really loonnng genre</span>
+              <span class="itemTags">Overflowing genre</span>
+          </div>
 
-        <div id="connectingDiv">
-            <span className="buyOptions">Edition *</span>
-            <span className="buyOptions">Type *</span>
-            <span className="buyOptions">Retailer *</span>
-            <span className="socialPlaceholderTemp"></span>
-            <span className="socialPlaceholderTemp"></span>
-            <span className="socialPlaceholderTemp"></span>
-        </div>
+          <div id="optionContainer">
+              <button class="buyingOption">Edition * </button>
+              <button class="buyingOption">Type * </button>
+              <button class="buyingOption">Retailer * </button>
+              <button class="buyingOption">1</button> {/*save/like*/}
+              <button class="buyingOption">2</button> {/*add to cart*/}
+              <button class="buyingOption">3</button> {/*share*/}
+          </div>
+        </span>
+      </div>
 
-        <div id="additionalInformationContainer">
+      <div id="additionalProductInfo">
+      <span id="leftStats">
+        <h3>Edition House</h3>
+        <p>This is an edition house</p>
 
-            <div id="additionalItemInformation">
-                <h3 id="editor">Edition House</h3>
-                <p>Thats an edition house</p>
+        <h3>Edition House</h3>
+        <p>Word Count : 0000</p>
+        <p>Page Count : 0000</p>
+        <p>Chapters : 0000</p>
+        <p>Editions : 0000</p>
 
-                <h3 id="funStats">Statistics</h3>
-                <p>Word Count : 0000</p>
-                <p>Page Count : 000</p>
-                <p>Chapters : 000</p>
-                <p>Editions : 000</p>
+        <h3>Languages</h3>
+        <p>Language 1</p>
+        <p>Language 2</p>
+      </span>
+      <span id="rightDescription">
+        <h3>Description</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+            <br/><br/> Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.</p>
+      </span>
+      </div>
 
-                <h3 id="itemLanguages">Languages</h3>
-                <p>American English</p>
-                <p>French</p>
-            </div>
+      <div id="otherProducts">
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
 
-            <div id="itemFullDescription">
-                <h3 id="description">Description</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel className aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                    <br/><br/> Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.</p>
-            </div> 
-        </div>
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
 
-        <h2 id="alsoLikeTitle">You might also like...</h2>
-        <br/>
-        <SmallProduct></SmallProduct>
-        
-        <div id="furtherStuff">
-            <img className="productImageSmall"
-            src={Image}
-            alt="product image"/>
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
 
-            <img className="productImageSmall"
-            src={Image}
-            alt="product image"/>
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
 
-            <img className="productImageSmall"
-            src={Image}
-            alt="product image"/>
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
 
-
-            <SmallProduct></SmallProduct>
-
-
-            <img className="productImageSmall"
-            src={Image}
-            alt="product image"/>
-
-            <img className="productImageSmall"
-            src={Image}
-            alt="product image"/>
-
-            <img className="productImageSmall"
-            src={Image}
-            alt="product image"/>
-        </div>  
-    </div>
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
+      </div>
+    </>
   );
 }
 
