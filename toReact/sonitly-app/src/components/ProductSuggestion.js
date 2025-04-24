5f7eede6e19cb20b7ed8c08d091c2b8e0be565ab
ash-a9236 <helene.m.rousseau@gmail.com>
2025-04-24 19:27:27 -0400

    making product suggestions as a component

---
+++ toReact/sonitly-app/src/components/ProductSuggestion.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Image from '../images/carousel-placeholder.jpg';
+
+const ProductSuggestion = () => {
+    <div id="otherProducts">
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
+
+        <img class="productImageSmall"
+        src={Image}
+        alt="product image"/>
+    </div>
+}
+
+export default ProductSuggestion;
