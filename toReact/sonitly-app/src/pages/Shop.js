4e32dd402383498d5896402eca97d96f6ffa4f9e
ash-a9236 <helene.m.rousseau@gmail.com>
2025-05-08 10:01:12 -0400

    start to merge

--- toReact/sonitly-app/src/pages/Shop.js
+++ toReact/sonitly-app/src/pages/Shop.js
@@ -1,5 +1,6 @@
 import ImagePlaceholder from '../images/image-placeholder.png';
 import Product from '../components/Product';
+import { Link } from 'react-router-dom';
 import { fetchData } from '../data/fetchWrapper'; 
 import { useEffect, useState } from "react";
 
@@ -21,12 +22,15 @@ const Shop = () => {
     <h2>Product Gallery</h2>
     <div id="gallery-container">
     {products.map((d) => (
-          <Product
-            key={d.item_id}
-            productName={d.item_title}
-            price={d.unit_price}
-            src={d.thumbnail_image || ImagePlaceholder}
-          />
+          <>
+            <Product
+              key={d.item_id}
+              productName={d.item_title}
+              price={d.unit_price}
+              src={d.thumbnail_image || ImagePlaceholder}
+            />
+            <Link to={`/shop/product/${d.item_id}`}>more info</Link>
+          </>
         ))}
     
     </div>
