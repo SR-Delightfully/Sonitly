74691dc125705c01e2c9c2447997356782a5c02d
sr-delightfully <delightfullysr@gmail.com>
2025-05-13 16:32:22 -0400

    added the base structure for random quotes generated from an API, and a music trivia game API, both of which are displayed on the explore page.

--- toReact/sonitly-app/src/pages/Explore.js
+++ toReact/sonitly-app/src/pages/Explore.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LeafletMap from "../components/LeafletMap";
+import MusicTriviaGame from "../components/MusicTriviaGame";
 
 const Explore = () => {
-  return (
+  const [stoicQuote, setStoicQuote] = useState(null);
 
-    <div>
-      <h1>Explore Locations</h1>
-    <div style={{display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <LeafletMap />
-    </div>
-    </div>
+  useEffect(()=> {
+    const fetchStoicQuote = async () => {
+      try {
+        const res = await fetch("https://stoic-quotes.com/api/quote");
+        const data = await res.json();
+        setStoicQuote(`"${data.text}" — ${data.author}`);
+      } catch {
+        setStoicQuote(null);
+      }
+    };
+
+    fetchStoicQuote();
+  }, []);
+
+  return (
+    <ol id="containers">
+      <li>
+        <div id="hero-container">
+          <h2>Explore Locations</h2>
+         
+          <div style={{display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <LeafletMap />
+          </div>
+        </div>
+      </li>
+      <li id="intro-container">
+        <div>
+          <h2>Take It Easy!</h2>
+          <span>
+            <h4>Think Stoic Thoughts:</h4>
+            <p>{stoicQuote}</p>
+          </span>
+        </div>
+        <div>
+         
+          <MusicTriviaGame />
+        </div>
+      </li>
+   
+    </ol>
   );
 }
 
