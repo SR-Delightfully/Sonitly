74f36e49eb519496e00a91a5da393997a3896aa9
kayyza <rachel.herron@live.ca>
2025-05-22 22:14:35 -0400

    added my api

--- toReact/sonitly-app/src/pages/Explore.js
+++ toReact/sonitly-app/src/pages/Explore.js
@@ -1,22 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { cache, useEffect, useState } from "react";
 import LeafletMap from "../components/LeafletMap";
 import MusicTriviaGame from "../components/MusicTriviaGame";
 
 const Explore = () => {
   const [stoicQuote, setStoicQuote] = useState(null);
+  const [advice, setAdvice] = useState(null);
 
-  useEffect(()=> {
+ 
+  useEffect(() => {
     const fetchStoicQuote = async () => {
       try {
         const res = await fetch("https://stoic-quotes.com/api/quote");
         const data = await res.json();
         setStoicQuote(`"${data.text}" — ${data.author}`);
       } catch {
-        setStoicQuote(null);
+        setStoicQuote("Could not load Stoic quote.");
       }
     };
 
     fetchStoicQuote();
+  }, []);
+
+
+  // rachel's api
+  const fetchAdvice = async () => {
+    try {
+      const res = await fetch("https://api.adviceslip.com/advice", {
+      cache: "no-store"
+    });
+      
+      const data = await res.json();
+      setAdvice(`"${data.slip.advice}"`);
+      
+    } catch {
+      setAdvice("Could not fetch advice. Try again later.");
+    }
+  };
+
+
+  useEffect(() => {
+    fetchAdvice();
   }, []);
 
   return (
@@ -24,28 +47,36 @@ const Explore = () => {
       <li>
         <div id="hero-container" className="overflowHidden">
           <h2 id="explore-title">Explore Locations</h2>
-         
-          <div style={{display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
             <LeafletMap />
           </div>
         </div>
       </li>
+
       <li id="intro-container">
         <div>
           <h2>Take It Easy!</h2>
+          
           <span>
             <h4>Think Stoic Thoughts:</h4>
             <p>{stoicQuote}</p>
           </span>
+
+          <div style={{ marginTop: "2rem" }}>
+            <h4>Need Advice?</h4>
+            <p>{advice}</p>
+            <button onClick={fetchAdvice} style={{ marginTop: "0.5rem" }}>
+              Get New Advice
+            </button>
+          </div>
         </div>
-        <div>
-         
+
+        <div style={{ marginTop: "3rem" }}>
           <MusicTriviaGame />
         </div>
       </li>
-   
     </ol>
   );
-}
+};
 
 export default Explore;
